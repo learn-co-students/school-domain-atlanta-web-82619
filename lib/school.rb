91625a7758b17a9c6a2b062151f69c23dd859089
Alexandria Pugia <alex.pugia816@hotmail.com>
2019-08-27 18:45:37 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
-# code here!
+class School
+  def initialize(name)
+    @roster = {}
+    @name = name
+  end
+  
+  def roster
+    @roster
+  end
+  
+  def add_student(student, grade)
+    @student = student
+    @grade = grade
+    if @roster[grade]
+      @roster[grade] << student
+    else
+      @roster[grade] = []
+      @roster[grade] << student
+    end
+  end
+  
+  def grade(grade)
+    return @roster[grade]
+  end
+  
+  def sort
+    @roster.each do |k, v|
+      v.sort!
+    end
+  end
+end
